0178f69b3df853e1aba5c7e78a48366ac0665be6
matthewcroughan <matthewcroughan@protonmail.com>
2020-02-12 16:51:30 +0000

    Add python script to turn the printer on or off which makes use of balena envvars

---
+++ octoprint/mqtton.py
@@ -0,0 +1,24 @@
+import paho.mqtt.client as paho
+import os
+import sys
+
+# Use environment variables from the Balena dashboard or any other source to inform the script.
+broker = os.environ["MQTT_BROKER"]
+port = os.environ["MQTT_BROKER_PORT"]
+espurna_name = os.environ["ESPURNA_RELAY_NAME"]
+# Create client object
+client = paho.Client("Octobalena")
+# Establish connection
+client.connect(broker,port)
+# Publish the MQTT message, second argument of client.publish() is the value of the message.
+try:
+  if sys.argv[1] == "on":
+    client.publish(espurna_name+"/relay/0/set","1")
+    print("Turned printed on")
+  elif sys.argv[1] == "off":
+    client.publish(espurna_name+"/relay/0/set","0")
+    print("Turned printer off")
+  else:
+    print("invalid or no argument(s) provided, valid arguments are 'on' or 'off'")
+except IndexError:
+  print("invalid or no argument(s) provided, valid arguments are 'on' or 'off'")
