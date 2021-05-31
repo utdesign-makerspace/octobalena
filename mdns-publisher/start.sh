82fe3a2f735d09c9df8041f819b73262fcfd9490
Daniel James <daniel@thzinc.com>
2021-05-31 12:23:46 -0700

    Fix bug in using OCTOPRINT_HOSTNAME to configure published hostname

--- mdns-publisher/start.sh
+++ mdns-publisher/start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# This takes the CONTAINER_HOSTNAME environment variable specified on the balena dashboard or other method and uses mdns-publish-cname to create a CNAME record on mDNS.
+# This takes the OCTOPRINT_HOSTNAME environment variable specified on the balena dashboard or other method and uses mdns-publish-cname to create a CNAME record on mDNS.
 # However, it sets a default of octobalena.local if nothing is provided.
 
-if [[ -z "$CONTAINER_HOSTNAME" ]]; then
+if [[ -z "$OCTOPRINT_HOSTNAME" ]]; then
   mdns-publish-cname octobalena.local
 else
   mdns-publish-cname ""$OCTOPRINT_HOSTNAME"".local
