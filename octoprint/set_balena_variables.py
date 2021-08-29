ccd02bba89f2fc316997d4a5788a029fddc13736
Cameron Steele <cam.steeleis@gmail.com>
2021-08-29 18:22:38 -0500

    Added LDAP and more ENV

--- octoprint/set_balena_variables.py
+++ octoprint/set_balena_variables.py
@@ -6,6 +6,13 @@ import os
 with open('./config.yaml', 'r+') as f:
     data = yaml.load(f, Loader=yaml.FullLoader)
     data["plugins"]["mqtt"]["broker"]["url"] = os.environ["MQTT_BROKER"]
+    data["plugins"]["auth_ldap"]["uri"] = os.environ["LDAP_URI"]
+    data["plugins"]["auth_ldap"]["auth_user"] = os.environ["LDAP_ADMIN_USER"]
+    data["plugins"]["auth_ldap"]["auth_password"] = os.environ["LDAP_ADMIN_PASS"]
+    data["plugins"]["auth_ldap"]["search_base"] = os.environ["LDAP_SEARCH_BASE"]
+    data["plugins"]["auth_ldap"]["ou"] = os.environ["LDAP_OU"]
+    data["apperance"]["name"] = os.environ["PRINTER_NAME"]
+    data["apperance"]["color"] = os.environ["PRINTER_COLOR"]
     f.seek(0)
     f.write(yaml.dump(data, default_flow_style=False))
     f.truncate()
